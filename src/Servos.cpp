e288be8d9afc2db1ea44d7f562379615ada0ff77
Victor Manuel Gomez Martinez <31774946+gmvm1@users.noreply.github.com>
2018-12-06 18:10:28 -0600

    Update Servos.cpp
    Bajar la velocidad de giro en su propio eje.

--- src/Servos.cpp
+++ src/Servos.cpp
@@ -99,13 +99,17 @@ void Servos::drive(float xIn, float yIn, float r) {
         m2Speed= yIn;
       }
       else if(yIn==0 && r!=0){
-        if(r>0){
-          m1Speed= r;
-          m2Speed= -r;
+        if(r>0.4){
+          m1Speed= 0.5*r;
+          m2Speed= -0.5*r;
         }
-        else if(r<0){
-          m1Speed= r;
-          m2Speed= -r;
+        else if(r<-0.4){
+          m1Speed= 0.5*r;
+          m2Speed= -0.5*r;
+        }
+        else{
+          m1Speed=0;
+          m2Speed=0;
         }
       }
   }
